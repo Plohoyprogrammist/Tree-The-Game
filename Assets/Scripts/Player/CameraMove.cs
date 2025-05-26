fb41c09245ea4199202b2daab7f5f9840cebd2b8
Plohoyprogrammist <plohoyprogrammist@gmail.com>
2025-05-26 19:30:32 +0700

    Сделал хуйню на свой манер (через жопу)

--- Assets/Scripts/Player/CameraMove.cs
+++ Assets/Scripts/Player/CameraMove.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     public int speed;
+    public float _mouseSensitivity;
+
+    private float _rotationY;
+    private float _rotationX;
 
     public GameObject pointCamera;
 
     private void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity;
+
+        _rotationY += mouseX;
+        _rotationX += mouseY;
+
+        transform.Rotate(Vector3.up, speed * mouseX * Time.deltaTime);
     }
 }
