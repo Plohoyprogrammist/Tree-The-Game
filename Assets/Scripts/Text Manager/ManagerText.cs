306467e52d0ce23a603306b5b121d7e7d8895f43
Plohoyprogrammist <plohoyprogrammist@gmail.com>
2025-05-26 10:51:19 +0700

    Идёт всё пока нормально. Заебись

---
+++ Assets/Scripts/Text Manager/ManagerText.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerText : MonoBehaviour
+{
+    public List<string> Texts = new List<string>();
+
+    public TextMeshProUGUI textUi;
+    public Image images;
+    private int index = 0;
+
+    private void Start()
+    {
+        textUi.text = Texts[index];
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            index++;
+            if (index > Texts.Count)
+            {
+                images.gameObject.SetActive(false);
+                index = 0;
+            }
+            else
+            {
+                textUi.text = Texts[index];
+            }
+        }
+    }
+}
